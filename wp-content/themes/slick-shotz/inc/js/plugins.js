3226cf48995ff597140a31254f24ae476a456c57
Rogelio Sanchez <rogeliosanchez@Rogelios-iMac.local>
2018-07-17 21:16:40 -0700

    added in js file with defer js function

---
+++ wp-content/themes/slick-shotz/inc/js/plugins.js
@@ -0,0 +1,26 @@
+function deferLoadingScript(url, callback) {
+    var loadScript = function() {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = url;
+
+        if (typeof callback === 'function') {
+            s.onload = s.onreadystatechange = function(event) {
+                event = event || window.event;
+                if (event.type === 'load' || (/loaded|complete/.test(s.readyState))) {
+                    s.onload = s.onreadystatechange = null;
+                        callback();
+                }
+            };
+        }
+        document.body.appendChild(s);
+    };
+    if (window.addEventListener) {
+        window.addEventListener('load', loadScript, false);
+    } else {
+        window.attachEvent('onload', loadScript);
+    }
+}
+
+/* Defer loading of a script */
+deferLoadingScript('https://www.youtube.com/yts/jsbin/www-embed-player-vflhai0Ku/www-embed-player.js');
--- wp-content/themes/slick-shotz/inc/register-styles-scripts.php
+++ wp-content/themes/slick-shotz/inc/register-styles-scripts.php
@@ -15,6 +15,7 @@ add_filter( 'wp_default_scripts', 'isa_remove_jquery_migrate' );
 
 function slick_shotz_enqueue_styles() {
 	wp_enqueue_style( 'skinnyfit-core', get_template_directory_uri().'/style.css');
+	wp_enqueue_script( 'plugins-js', get_template_directory_uri().'/inc/js/plugins.js', array('jquery'), '1.0.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'slick_shotz_enqueue_styles' );
 ?>
